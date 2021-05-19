f57b946c4ee40b9ce6e920f152eaafbcc7c29910
arif <arif@jubi.ai>
2021-05-19 17:53:14 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bajaj Finserv support tools Devloping</p>
+        <p>sachin tools Devloping</p>
       </header>
     </div>
   );
